ed0bdad6f70708bef8eea21da522358d076180fc
Phan Thị Ngọc Mai <inet.ngocmai.v6@gmail.com>
2025-05-31 05:09:30 +0700

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,28 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ['res.cloudinary.com'],
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['res.cloudinary.com'],
+module.exports = {
+  compiler: {
+    // Remove all console logs
+    removeConsole: false
   },
+  images: {
+    domains: ['res.cloudinary.com', 'www.youtube.com', 'img.youtube.com'],
+  },
+  devIndicators: {
+    position: false
+  }
+};
+
+const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
